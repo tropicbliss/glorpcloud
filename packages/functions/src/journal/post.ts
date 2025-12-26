bab653022388b548f8e2417232c4b37c0f38f60f
sadgedup <tropicbliss@protonmail.com>
2025-12-26 11:56:25 +0800

    Update post.ts

--- packages/functions/src/journal/post.ts
+++ packages/functions/src/journal/post.ts
@@ -18,35 +18,8 @@ export const main = Util.handler(async (input) => {
             Item: {
                 userId: input.userId,
                 entry: body.date,
-                content: JSON.stringify({
-                    root: {
-                        children: [
-                            {
-                                children: [
-                                    {
-                                        detail: 0,
-                                        format: 0,
-                                        mode: "normal",
-                                        style: "",
-                                        text: "",
-                                        type: "text",
-                                        version: 1,
-                                    },
-                                ],
-                                direction: "ltr",
-                                format: "",
-                                indent: 0,
-                                type: "paragraph",
-                                version: 1,
-                            },
-                        ],
-                        direction: "ltr",
-                        format: "",
-                        indent: 0,
-                        type: "root",
-                        version: 1,
-                    },
-                }),
+                content:
+                    '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}',
             },
         }),
     );
